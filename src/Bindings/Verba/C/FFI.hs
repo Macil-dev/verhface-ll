b462980b3125ac9dede84f13ad9cf3527d2aaae8
Macil <Macil.dev@gmail.com>
2014-04-09 10:25:18 +0400

    add implement memory-based procedures
    encryption procedures use different container format
    do not use them if you don't know what you're doing

--- src/Bindings/Verba/C/FFI.hs
+++ src/Bindings/Verba/C/FFI.hs
@@ -47,21 +47,77 @@ foreign import stdcall "DeCryptFile" c_DeCryptFile :: CString    -- ^ Source fil
                                                    -> CString    -- ^ Destination filename
                                                    -> Word16     -- ^ Key id
                                                    -> IO Word16
+-- | EnCryptMem
+foreign import stdcall "EnCryptMem" c_EnCryptMem :: Ptr Word8     -- ^ Source buffer
+                                                 -> Word32        -- ^ Source buffer length
+                                                 -> Ptr Word8     -- ^ Destination buffer
+                                                 -> Word16        -- ^ Sender key ID
+                                                 -> Ptr Word16    -- ^ Receivers key ids
+                                                 -> CString       -- ^ Key series
+                                                 -> IO Word16
+-- | DeCryptMem
+foreign import stdcall "DeCryptMem" c_DeCryptMem :: Ptr Word8      -- ^ Buffer
+                                                 -> Ptr Word32     -- ^ Buffer length
+                                                 -> Word16         -- ^ Receiver key id
+                                                 -> IO Word16
 -- * Signing
 -- | SignFile
 foreign import stdcall "SignFile" c_SignFile :: CString   -- ^ Source filename
                                              -> CString   -- ^ Destination filename
                                              -> CString   -- ^ Key id
                                              -> IO Word16
+-- | SignMem
+foreign import stdcall "SignMem" c_SignMem :: Ptr Word8   -- ^ Buffer
+                                           -> Word32      -- ^ BufferLength
+                                           -> CString     -- ^ Key id
+                                           -> IO Word16
+-- | SignMemEx
+foreign import stdcall "SignMemEx" c_SignMemEx :: Ptr Word8   -- ^ Buffer
+                                               -> Ptr Word32  -- ^ BufferLength
+                                               -> CString     -- ^ Key id
+                                               -> IO Word16
+-- | SignMemSeparate
+foreign import stdcall "SignMemSeparate" c_SignMemSeparate :: Ptr Word8  -- ^ Input buffer
+                                                           -> Word32     -- ^ Input buffer length
+                                                           -> Word32     -- ^ Output buffer length
+                                                           -> CString    -- ^ Key id
+                                                           -> Ptr Word8  -- ^ Output buffer
+                                                           -> IO Word16
+-- | SignMemSeparateEx
+foreign import stdcall "SignMemSeparateEx" c_SignMemSeparateEx :: Ptr Word8  -- ^ Input buffer
+                                                               -> Word32     -- ^ Input buffer length
+                                                               -> Ptr Word32 -- ^ Output buffer length
+                                                               -> CString    -- ^ Key id
+                                                               -> Ptr Word8  -- ^ Output buffer
+                                                               -> IO Word16
 -- | check_file_sign
 foreign import stdcall "check_file_sign" c_check_file_sign :: CString     -- ^ Signed filename
                                                            -> Ptr Word8   -- ^ Size of result
                                                            -> Ptr (Ptr C_Check_Status) -- ^ Result
                                                            -> IO Word16
+-- | check_mem_sign
+foreign import stdcall "check_mem_sign" c_check_mem_sign :: Ptr Word8     -- ^ Input buffer
+                                                         -> Word32        -- ^ Input buffer length
+                                                         -> Ptr Word8     -- ^ Size of result
+                                                         -> Ptr (Ptr C_Check_Status) -- ^ Result
+                                                         -> IO Word16
+-- | CheckMemSeparate
+foreign import stdcall "CheckMemSeparate" c_CheckMemSeparate :: Ptr Word8  -- ^ Input buffer (data)
+                                                             -> Word32     -- ^ Size of data
+                                                             -> Word32     -- ^ Size of signature
+                                                             -> Ptr Word8  -- ^ Size of result
+                                                             -> Ptr (Ptr C_Check_Status) -- ^ Result
+                                                             -> Ptr Word8  -- ^ Input buffer (signature)
+                                                             -> IO Word16
 -- | DelSign
 foreign import stdcall "DelSign" c_DelSign :: CString   -- ^ Signed filename
                                            -> Word8     -- ^ Number of signatires to remove (-1 to remove all)
                                            -> IO Word16
+-- | Del_Mem_Sign
+foreign import stdcall "Del_Mem_Sign"  c_Del_Mem_Sign :: Ptr Word8   -- ^ Buffer
+                                                      -> Ptr Word32  -- ^ Buffer length
+                                                      -> Word8       -- ^ Number of signatures to remove (-1 to remove all)
+                                                      -> IO Word16
 
 -- * Miscellaneous
 -- | GetDrvInfo
@@ -74,6 +130,18 @@ foreign import stdcall "GetFileSenderID" c_GetFileSenderId :: CString    -- ^ En
                                                            -> IO Word16
 -- | GetCryptKeysF
 foreign import stdcall "GetCryptKeysF" c_GetCryptKeysF :: CString           -- ^ Encrypted filename
+                                                       -> Ptr Word16        -- ^ Size of result array
+                                                       -> Ptr (Ptr Word16)  -- ^ Result (array of keys)
+                                                       -> CString           -- ^ Result (key series)
+                                                       -> IO Word16
+-- | GetMemSenderId
+foreign import stdcall "GetMemSenderID" c_GetMemSenderID :: Ptr Word8       -- ^ Input buffer
+                                                         -> Word32          -- ^ Buffer length
+                                                         -> CString         -- ^ Result
+                                                         -> IO Word16
+-- | GetCryptKeysM
+foreign import stdcall "GetCryptKeysM" c_GetCryptKeysM :: Ptr Word8         -- ^ Input buffer
+                                                       -> Word32            -- ^ Buffer length
                                                        -> Ptr Word16        -- ^ Size of result array
                                                        -> Ptr (Ptr Word16)  -- ^ Result (array of keys)
                                                        -> CString           -- ^ Result (key series)
